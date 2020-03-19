689d4aae2c28895c290a021158ddbf84bb6ed2c7
shadhini <jshadhiniaseka@gmail.com>
2020-03-20 01:12:21 +0530

    implement script to retrieve data from covidsl.com and write it to csv file

--- corona_data/corona_data/spiders/IFS_spider.py
+++ corona_data/corona_data/spiders/IFS_spider.py
@@ -2,6 +2,16 @@ import scrapy
 from datetime import datetime, timedelta
 
 
+def write_to_file(file_name, data):
+    with open(file_name, 'w+') as f:
+        f.write('\n'.join(data))
+
+
+def append_to_file(file_name, data):
+    with open(file_name, 'a+') as f:
+        f.write('\n'.join(data))
+
+
 class IFSSpider(scrapy.Spider):
     name = "ifs"
 
@@ -14,9 +24,29 @@ class IFSSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # page = response.url.split("/")[-2]
-        page = (datetime.now()).strftime("%Y-%m-%d_%H-%M-%S")
-        filename = 'ifs-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        file_name = ("IFS_{}".format(response.xpath('//*[@id="614137682"]/div/table/tbody/tr[3]/td[1]/text()').getall()[0]))\
+            .replace(":", "-").replace(", ", "_").replace(" ", "_")
+
+        col_index = [1, 2, 4, 5, 6, 7, 8, 9,11]
+
+        write_to_file(file_name, ['Patient_No, Confirmed_Date, Age, Gender, Residence_City, Detected_City, '
+                                  'Detected_Prefecture, Status, Notes', ''])
+
+        data = ['Patient_No, Confirmed_Date, Age, Gender, Residence_City, Detected_City, Detected_Prefecture, Status, Notes']
+
+        length = int(response.xpath('//*[@id="0"]/div/table/tbody/tr/td[1]/text()').getall()[0])
+
+        for i in range (1, length+2):
+            row = []
+            for j in col_index:
+                list = response.xpath('//*[@id="0"]/div/table/tbody/tr[{}]/td[{}]/text()'.format(i, j)).getall()
+                if len(list) > 0:
+                    row.append(list[0])
+                else:
+                    row.append('')
+
+            data.append(','.join(row))
+
+        write_to_file(file_name, data)
+
+
