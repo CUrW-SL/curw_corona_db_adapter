4f76aae90e6abb16efe2c907b2d46703f84e36fc
shadhini <jshadhiniaseka@gmail.com>
2020-03-20 16:51:22 +0530

    solve module import issue

--- db_utils/update_corona_data.py
+++ db_utils/update_corona_data.py
@@ -5,6 +5,11 @@ import json
 import os
 import csv
 
+
+import sys
+sys.path.insert(0, '/home/uwcc-admin/curw_corona_db_adapter/db_utils')
+import db_utils
+
 from db_utils.base import get_Pool, destroy_Pool
 from db_utils.patient_data import insert_patient_data, bulk_insert_patient_data_customized, bulk_insert_patient_data
 
@@ -41,7 +46,7 @@ if __name__=="__main__":
         CURW_CORONA_HOST = read_attribute_from_config_file('CURW_CORONA_HOST', config)
         CURW_CORONA_PORT = read_attribute_from_config_file('CURW_CORONA_PORT', config)
         CURW_CORONA_DATABASE = read_attribute_from_config_file('CURW_CORONA_DATABASE', config)
-        
+
         pool = get_Pool(host=CURW_CORONA_HOST, user=CURW_CORONA_USERNAME, password=CURW_CORONA_PASSWORD,
                         port=CURW_CORONA_PORT, db=CURW_CORONA_DATABASE)
 
