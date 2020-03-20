28218b191da7309c4f5b6fe1b8a4485502eb3b22
shadhini <jshadhiniaseka@gmail.com>
2020-03-20 16:39:15 +0530

    debug IFS data extraction

--- corona_data2/get_hpb_data.py
+++ corona_data2/get_hpb_data.py
@@ -1,2 +1,7 @@
 import requests
 import json
+
+url = "http://www.hpb.health.gov.lk/api/get-current-statistical"
+
+response = requests.get(url)
+
--- db_utils/__init__.py
+++ db_utils/__init__.py
@@ -1,2 +1,3 @@
-
+from .patient_data import bulk_insert_patient_data_customized, bulk_insert_patient_data, insert_patient_data
+from .base import get_Pool, destroy_Pool, get_connection_for_iterable_cursor
 
